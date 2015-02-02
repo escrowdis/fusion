04cfd434b51fd5aaabe3231e97d3dd0a02625da1
Tony Weng <eqttonytony@gmail.com>
2015-02-02 22:11:52 +0800

    sys: fixing. mouse control

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -163,9 +163,11 @@ void MouseLabel::mouseMoveEvent(QMouseEvent *e)
 
 void MainWindow::mouseXY(int x, int y)
 {
+    int xx = 2 * x;
+    int yy = 2 * y;
     mouse_info.sprintf("(x,y) = (%d,%d), Disp. = %d, (X,Y,Z) = (%d,%d,%d)",
-                       2 * x, 2 * y, sv->data[y][x].disp,
-                       -1, -1, sv->data[y][x].Z); //**// real X, Y, Z
+                       xx, yy, sv->data[yy][xx].disp,
+                       -1, -1, sv->data[yy][xx].Z); //**// real X, Y, Z
     ui->label_depth_info->setText(mouse_info);
 }
 
